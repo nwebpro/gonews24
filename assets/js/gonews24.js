4c7bb6d5b99bc8a3db4e4e02ba3af7538d153eee
nwebpro <nwebpro.work@gmail.com>
2022-09-02 23:49:49 +0600

    Go News 24 Get Category Id daynamicly

--- assets/js/gonews24.js
+++ assets/js/gonews24.js
@@ -10,19 +10,32 @@ const loadNewsCategory = async() => {
 }
 
 const setCategory = category => {
-    console.log(category);
     const catDisplay = document.getElementById('cat-display');
     category.forEach(cat => {
-        const {category_name} = cat;
+        const {category_name, category_id} = cat;
         const li = document.createElement('li');
         li.innerHTML = `
-            <a href="#" class="block py-2 pr-4 pl-3 mb-2 text-white bg-blue-700 rounded md:bg-transparent md:text-black md:p-0 dark:text-white">${category_name}</a>
+            <a href="#" onclick="loadPostDetails('${category_id}')" class="block py-2 pr-4 pl-3 mb-2 text-white bg-blue-700 rounded md:bg-transparent md:text-black md:p-0 dark:text-white">${category_name}</a>
         `;
         catDisplay.appendChild(li);
     })
 }
 
+const loadPostDetails = async cat_id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${cat_id}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayPostDetails(data.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+const displayPostDetails = posts => {
+    console.log(posts);
+
+}
 
 
 
